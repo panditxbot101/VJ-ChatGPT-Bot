87b54f60635f1d0ee787e30244a67932758f6f20
panditxbot101 <155144919+panditxbot101@users.noreply.github.com>
2023-12-29 16:39:38 +0530

    Update config.py

--- config.py
+++ config.py
@@ -10,12 +10,12 @@ def is_enabled(value, default):
     else:
         return default
 
-API_ID = int(environ.get("API_ID", ""))
-API_HASH = environ.get("API_HASH", "")
-BOT_TOKEN = environ.get("BOT_TOKEN", "")
-LOG_CHANNEL = int(environ.get("LOG_CHANNEL", ""))
-ADMINS = int(environ.get("ADMINS", ""))
-DB_URI = environ.get("DB_URI", "")
+API_ID = int(environ.get("API_ID", "26558147"))
+API_HASH = environ.get("API_HASH", "ff188f7a9a2e29730d285afb200393dc")
+BOT_TOKEN = environ.get("BOT_TOKEN", "6825090297:AAF28OlBRFouPfRbnMiPe0jdTOYPJMbx7G0")
+LOG_CHANNEL = int(environ.get("LOG_CHANNEL", "-1002053554673"))
+ADMINS = int(environ.get("ADMINS", "6229151120"))
+DB_URI = environ.get("DB_URI", "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")
 DB_NAME = environ.get("DB_NAME", "chatgptvjbot")
 OPENAI_API = environ.get("OPENAI_API", "")
 AI = is_enabled((environ.get("AI","True")), False)
